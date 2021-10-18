30854e2e9c6e9bce7982b7cb9cbd2cf61e5c7559
Jaidev Chattre <jaidevchattre@gmail.com>
2021-10-18 19:54:48 +0530

    Initial commit

--- src/test/java/com/springboot_jpatest/demo/SpringbootJPAApplicationTests.java
+++ src/test/java/com/springboot_jpatest/demo/SpringbootJPAApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springboot_hiibernate.demo;
+package com.springboot_jpatest.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootHibernateApplicationTests {
+class SpringbootJPAApplicationTests {
 
 	@Test
 	void contextLoads() {
